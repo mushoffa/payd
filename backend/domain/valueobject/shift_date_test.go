e21c46fee31c7a6a1d635d5cee53736027d4d883
mushoffa <ridwan.mushoffa@gmail.com>
2025-05-21 17:35:45 +0700

    backend(test): Add ShiftDate backdate test case

--- backend/domain/valueobject/shift_date_test.go
+++ backend/domain/valueobject/shift_date_test.go
@@ -13,6 +13,31 @@ type shift_date struct {
 	Date ShiftDate `json:"date"`
 }
 
+func Test_ShiftDate_Validate_Today(t *testing.T) {
+	// Given
+	today := time.Now()
+	shift := NewShiftDate(today)
+
+	// When
+	err := shift.Validate()
+
+	// Then
+	assert.Nil(t, err)
+}
+
+func Test_ShiftDate_Validate_Yesterday(t *testing.T) {
+	// Given
+	yesterday := time.Now().Add(-1 * time.Hour *24)
+	shift := NewShiftDate(yesterday)
+
+	// When
+	err := shift.Validate()
+
+	// Then
+	assert.NotNil(t, err)
+	assert.ErrorIs(t, err, ErrInvalidDate)
+}
+
 func Test_ShiftDate_UnmarshalJSON_Success(t *testing.T) {
 	// Given
 	var date shift_date
