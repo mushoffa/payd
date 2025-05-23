482b603e19dfe7cd29f33280c5530d6676c1daf6
mushoffa <ridwan.mushoffa@gmail.com>
2025-05-23 10:38:57 +0700

    backend(shift): Add database conn interface

--- backend/main.go
+++ backend/main.go
@@ -1,25 +1,17 @@
 package main
 
 import (
-	"fmt"
-
-	// "payd/application"
 	"payd/infrastructure/database"
 	"payd/infrastructure/http"
-	// "payd/infrastructure/repository"
 	"payd/shift"
 )
 
 func main() {
-	database.NewPostgresClient("admin", "admin", "localhost", 5442, "payd", false)
-	// shift_r := repository.NewShiftRepository(db)
-
-	// usecase := application.NewUsecase(shift_r)
+	postgres := database.NewPostgresClient("admin", "admin", "localhost", 5442, "payd", false)
 	server := http.NewServer(9095)
 
-	shift.New(server)
+	shift.New(server, postgres)
 	server.Run()
 
-	fmt.Println("Close database here")
-	// db.Close()
+	postgres.Close()
 }
--- backend/shift/shift.go
+++ backend/shift/shift.go
@@ -1,11 +1,14 @@
 package shift
 
 import (
+	"payd/infrastructure/database"
 	"payd/infrastructure/http"
 	"payd/shift/handler"
+
+	"github.com/jackc/pgx/v5"
 )
 
-func New(server http.HttpServer) {
+func New(server http.HttpServer, db database.DatabaseService[*pgx.Conn]) {
 	s := server.GetInstance()
 	handler.ShiftRouter(s)
 }
