067cc8e4c6071febb0b80c1a7b3d79700fcdc5e7
mushoffa <ridwan.mushoffa@gmail.com>
2025-06-05 13:11:45 +0700

    backend(infrastructure): Refactor database interface, remove generic type

--- backend/infrastructure/database/database.go
+++ backend/infrastructure/database/database.go
@@ -1,6 +1,15 @@
 package database
 
-type DatabaseService[T any] interface {
-	GetInstance() T
+import (
+	"context"
+)
+
+type DatabaseService interface {
+	Insert(context.Context, string, ...any) (int, error)
+	QueryOne(context.Context, string, ...any) (any, error)
+	QueryMany(context.Context, string, ...any) (any, error)
+	Exec(context.Context, string, ...any) (any, error)
+	Lock(context.Context, int) error
+	Unlock(context.Context, int) error
 	Close()
 }
