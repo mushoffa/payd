16905454d75f71aba7515f88cb7e964c063c8d31
mushoffa <ridwan.mushoffa@gmail.com>
2025-06-13 15:22:31 +0700

    backend(shift): Add api endpoint and route registration function

--- backend/shift/handler/handler.go
+++ backend/shift/handler/handler.go
@@ -7,24 +7,45 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+const (
+	v1_api = "/api/v1/shifts"
+	v2_api = "/api/v2/shifts"
+)
+
+type ShiftHandler interface {
+	RegisterV1() (string, *fiber.App)
+	RegisterV2() (string, *fiber.App)
+}
+
 type shift struct {
 	http.FiberHandler
 	r domain.ShiftRepository
 }
 
-func NewShiftHandler(r domain.ShiftRepository) *shift {
-	return &shift{r: r}
+func NewShiftHandler(r domain.ShiftRepository) ShiftHandler {
+	h := &shift{r: r}
+	h.Init()
+	return h
 }
 
-func Routes(r domain.ShiftRepository) *fiber.App {
+func (h *shift) RegisterV1() (string, *fiber.App) {
 	shifts := fiber.New()
+	shifts.Post("/", h.create())
+	shifts.Get("/", h.getAll())
+	shifts.Get("/:id", h.getByID())
+	shifts.Put("/:id", h.update())
+	shifts.Delete("/:id", h.delete())
 
-	shift := NewShiftHandler(r)
-	shifts.Post("/", shift.create())
-	shifts.Get("/", shift.getAll())
-	shifts.Get("/:id", shift.getByID())
-	shifts.Put("/:id", shift.update())
-	shifts.Delete("/:id", shift.delete())
+	return v1_api, shifts
+}
 
-	return shifts
+func (h *shift) RegisterV2() (string, *fiber.App) {
+	shifts := fiber.New()
+	shifts.Post("/", h.create())
+	shifts.Get("/", h.getAll())
+	shifts.Get("/:id", h.getByID())
+	shifts.Put("/:id", h.update())
+	shifts.Delete("/:id", h.delete())
+
+	return v2_api, shifts
 }
