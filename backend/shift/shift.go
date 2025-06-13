2331a03dadd340c236fd6e1cf0620b329b42a20e
mushoffa <ridwan.mushoffa@gmail.com>
2025-06-13 15:30:37 +0700

    backend(shift): Add handler function to register router on http server

--- backend/shift/shift.go
+++ backend/shift/shift.go
@@ -4,11 +4,18 @@ import (
 	"payd/infrastructure/database"
 	"payd/shift/handler"
 	"payd/shift/infrastructure/repository"
-
-	"github.com/gofiber/fiber/v2"
 )
 
-func New(database database.DatabaseService) *fiber.App {
+type Shift struct {
+	handlres handler.ShiftHandler
+}
+
+func New(database database.DatabaseService) *Shift {
 	r := repository.NewShiftsRepository(database)
-	return handler.Routes(r)
+	h := handler.NewShiftHandler(r)
+	return &Shift{h}
+}
+
+func (s *Shift) Handler() handler.ShiftHandler {
+	return s.handlres
 }
