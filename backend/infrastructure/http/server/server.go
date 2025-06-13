40bc552526cb7bc05f4042d706f7bb4d27ff4acb
mushoffa <ridwan.mushoffa@gmail.com>
2025-06-13 15:14:07 +0700

    backend(infrastructure): Add new function for adding middleware & router on server

--- backend/infrastructure/http/server/server.go
+++ backend/infrastructure/http/server/server.go
@@ -9,13 +9,17 @@ import (
 
 	"payd/infrastructure/http/server/middleware"
 
+	"github.com/gofiber/contrib/otelfiber"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
+type routeFn func() (string, *fiber.App)
+
 type HttpServer interface {
-	AddRoute(string, *fiber.App)
+	AddRoutes(...routeFn)
+	AddMiddleware(...fiber.Handler)
 	Start() error
 	Shutdown(context.Context) error
 }
@@ -23,14 +27,6 @@ type HttpServer interface {
 type server struct {
 	instance *fiber.App
 	port     int
-	routes   router
-}
-
-type router struct {
-	v1 fiber.Router
-	// Extend the version api router here:
-	//   v2 fiber.Router
-	// Add new router(s) accordingly at instantiation function
 }
 
 func NewServer(port int) HttpServer {
@@ -39,28 +35,32 @@ func NewServer(port int) HttpServer {
 		WriteTimeout: 10 * time.Second,
 	})
 
-	// Middleware
-	app.Use(cors.New())
-	app.Use(logger.New())
-	app.Use(middleware.ErrorHandler)
-
-	// Router
-	api := app.Group("/api")
-	v1 := api.Group("/v1")
-
-	r := router{
-		v1: v1,
-	}
-
-	return &server{
+	s := &server{
 		instance: app,
 		port:     port,
-		routes:   r,
+	}
+
+	s.AddMiddleware(
+		cors.New(),
+		logger.New(),
+		middleware.ErrorHandler,
+		otelfiber.Middleware(),
+	)
+
+	return s
+}
+
+func (s *server) AddMiddleware(fn ...fiber.Handler) {
+	for _, f := range fn {
+		s.instance.Use(f)
 	}
 }
 
-func (s *server) AddRoute(name string, router *fiber.App) {
-	s.routes.v1.Mount(name, router)
+func (s *server) AddRoutes(fn ...routeFn) {
+	for _, f := range fn {
+		name, router := f()
+		s.instance.Mount(name, router)
+	}
 }
 
 func (s *server) Start() error {
@@ -73,7 +73,6 @@ func (s *server) Start() error {
 
 func (s *server) Shutdown(ctx context.Context) error {
 	if err := s.instance.ShutdownWithContext(ctx); err != nil {
-		fmt.Println("Err: ", err)
 		return err
 	}
 
